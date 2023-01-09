3111af417a6d49131315a3dd0eab6ff748e41d9f
dlstupakov <ultrastup1@yandex.ru>
2023-01-09 10:01:10 +0300

    init project

---
+++ src/test/java/ru/stupakov/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stupakov.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
